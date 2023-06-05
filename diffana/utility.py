c524b5335f7cc4817ccb3dfa1c70307ee4032238
MelissaVu-24 <73518333+MelissaVu-24@users.noreply.github.com>
2023-06-05 03:12:30 -0700

    Update utility.py

--- diffana/utility.py
+++ diffana/utility.py
@@ -15,17 +15,17 @@ def volcano(names, fold_change, pval):
 	----------
 	Volcano Plot
 	"""
-	plt.scatter(x=fold_change,y=pval.apply(lambda x:-np.log10(x)),s=1)
+	plt.scatter(x=fold_change,y=pval,s=1)
 	# highlight down- or up- regulated genes
-	size = fold_change.len()
-	down_names[size]
-	down_fc[size]
-	down_pv[size]
-	up_fc[size]
-	up_pv[size]
-	up_names[size]
+	size = len(fold_change)
+	down_names = []
+	down_fc = []
+	down_pv = []
+	up_fc = []
+	up_pv = []
+	up_names = []
 	
-	for i in fold_change.len():
+	for i in range(size):
 		if fold_change[i] <= 0:
 			down_fc.append(fold_change[i])
 			down_pv.append(pval[i]) 
@@ -35,14 +35,15 @@ def volcano(names, fold_change, pval):
 			up_pv.append(pval[i])
 			up_names.append(names[i])
 
-	plt.scatter(x=down_fc,y=down_pv.apply(lambda x:-np.log10(x)),s=3,label="Down-regulated",color="blue")
-	plt.scatter(x=up_fc,y=up_pv.apply(lambda x:-np.log10(x)),s=3,label="Up-regulated",color="red")
+	plt.scatter(x=down_fc,y=down_pv,s=3,label="Down-regulated",color="blue")
+    	plt.scatter(x=up_fc,y=up_pv,s=3,label="Up-regulated",color="red")
   
-	for i in down_fc.len():
+	#Label names of differentially expressed genes up- or down regulated
+	for i in range(len(down_fc)):
 		if -np.log10(down_pv[i]) > 20:
 			plt.text(x=down_fc[i],y=-np.log10(down_pv[i]),s=down_names[i],fontdict=dict(color='blue',size=6))
       
-	for i in up_fc.len():
+	for i in range(len(up_fc)):
 		if -np.log10(up_pv[i]) > 20:
 			plt.text(x=up_fc[i],y=-np.log10(up_pv[i]),s=up_names[i],fontdict=dict(color='blue',size=6))
 
